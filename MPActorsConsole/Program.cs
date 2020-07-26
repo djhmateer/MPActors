1b5883d95c18baf920ed91303745ceae62b385b4
Dave Mateer <davemateer@gmail.com>
2020-07-26 20:10:00 +0100

    auto commit

---
+++ MPActorsConsole/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace MPActorsConsole
+{
+    class ProgramA
+    {
+        public static void MainA()
+        {
+            var actors = GetActors();
+
+            foreach (var actor in actors)
+            {
+                Console.WriteLine(actor);
+            }
+        }
+
+        public static IEnumerable<Actor> GetActors()
+        {
+            using (var connection = GetOpenConnection())
+            {
+                return connection.Query<Actor>(
+                    @"SELECT TOP 10 *
+                    FROM Actors");
+            }
+        }
+
+        public static IDbConnection GetOpenConnection()
+        {
+            var connection = new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=IMDBChallenge;Trusted_Connection=True;MultipleActiveResultSets=true");
+            connection.Open();
+            return connection;
+        }
+
+        public class Actor
+        {
+            public int actorid { get; set; }
+            public string name { get; set; }
+            public string sex { get; set; }
+
+            public override string ToString() => $"{actorid} {name} {sex}";
+        }
+
+    }
+}
---
+++ MPActorsConsole/ProgramAsync.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace MPActorsConsole
+{
+    class ProgramAs
+    {
+        public static async Task MainAs()
+        {
+            Console.WriteLine("Program Async test");
+            var actors = await GetActorsAsync();
+
+            foreach (var actor in actors)  Console.WriteLine(actor); 
+        }
+
+        public static async Task<IEnumerable<Actor>> GetActorsAsync()
+        {
+            using var connection = await GetOpenConnectionAsync();
+            return await connection.QueryAsync<Actor>(
+                @"SELECT TOP 10 *
+                FROM Actors");
+        }
+
+        public static async Task<IDbConnection> GetOpenConnectionAsync()
+        {
+            var connection = new SqlConnection(ConnectionString);
+            await connection.OpenAsync();
+            return connection;
+        }
+
+        public class Actor
+        {
+            public int actorid { get; set; }
+            public string name { get; set; }
+            public string sex { get; set; }
+
+            public override string ToString() => $"{actorid} {name} {sex}";
+        }
+
+        private const string ConnectionString = "Server=(localdb)\\mssqllocaldb;Database=IMDBChallenge;Trusted_Connection=True;MultipleActiveResultSets=true";
+    }
+}
