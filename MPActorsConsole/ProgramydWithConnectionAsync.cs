6deb4aed180f0605f474f85ea0b561a1ecf6c1f5
Dave Mateer <davemateer@gmail.com>
2020-07-31 16:04:15 +0100

    auto commit

--- MPActorsConsole/ProgramydWithConnectionAsync.cs
+++ MPActorsConsole/ProgramydWithConnectionAsync.cs
@@ -4,17 +4,38 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
 using Dapper;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace MPActorsConsole
 {
-    class ProgramMPA
+    public class MPATests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly string connectionString;
+
+        public MPATests(ITestOutputHelper output)
+        {
+            this.output = output;
+            connectionString = "Server=(localdb)\\mssqllocaldb;Database=IMDBChallengeTest;Trusted_Connection=True;MultipleActiveResultSets=true";
+        }
+
+        [Fact]
+        public void ShouldBeAbleToGetActors()
+        {
+            output.WriteLine("test message");
+
+            var result = await ProgramMPA.GetActors()
+            Assert.Equal(2,2);
+        }
+    }
+
+    public class ProgramMPA
     {
         private const string ConnectionString = "Server=(localdb)\\mssqllocaldb;Database=IMDBChallenge;Trusted_Connection=True;MultipleActiveResultSets=true";
 
         public static async Task Main()
         {
-            Console.WriteLine("Experimenting with a HOF for db connection and using statements");
-            Console.WriteLine("so don't have code duplication");
             var actors = await GetActors(ConnectionString);
 
             foreach (var actor in actors) Console.WriteLine(actor);
@@ -22,11 +43,11 @@ namespace MPActorsConsole
 
         // Function 
         public static async Task<IEnumerable<Actor>> GetActors(string connectionString)
-            => await WithConnection(connectionString, async conn =>
+            => await WithConnection(connectionString, async x =>
             {
-                var result = await conn.QueryAsync<Actor>(
+                var result = await x.QueryAsync<Actor>(
                     @"SELECT TOP 10 *
-                   FROM Actors");
+                    FROM Actors");
                 return result;
             });
 
@@ -36,114 +57,10 @@ namespace MPActorsConsole
             string connectionString,
             Func<IDbConnection, Task<T>> func)
         {
-            using var conn = new SqlConnection(connectionString);
-
-            //conn.OpenAsync();
-
+            await using var conn = new SqlConnection(connectionString);
             return await func(conn);
         }
 
-        //    return connection;
-        //}
-
-        //public static Task<IEnumerable<Actor>> GetActorsx()
-        //{
-        //    // passing a lambda expression (function) into 
-        //    //var foo = WithConnection(async conn =>
-        //    //{
-        //    //    var result = await conn.QueryAsync<Actor>(
-        //    //        @"SELECT TOP 10 * 
-        //    //        FROM actors");
-        //    //    return result;
-        //    //});
-        //    var foo = WithConnection(Bar);
-
-        //    return foo;
-        //}
-
-        // function that does something
-        // we are passing in an IDbConnection
-        //private static async Task<IEnumerable<Actor>> Bar(IDbConnection conn)
-        //{
-        //    var result = await conn.QueryAsync<Actor>(@"SELECT TOP 10 * 
-        //            FROM actors");
-        //    return result;
-        //}
-
-        //// Wrapper function
-        //// does the connection to the db (
-        //private static async Task<T> WithConnection<T>(
-        //    Func<IDbConnection, Task<T>> connectionFunction)
-        //{
-        //    using (var conn = new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=IMDBChallenge;Trusted_Connection=True;MultipleActiveResultSets=true"))
-        //    {
-        //        conn.Open();
-        //        return await connectionFunction(conn);
-        //    }
-        //}
-
-        //private static async Task WithConnection(
-        //    string connectionString,
-        //    Func<IDbConnection, Task> connectionFunction)
-        //{
-        //    // await using?
-        //    using (var conn = new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=IMDBChallenge;Trusted_Connection=True;MultipleActiveResultSets=true"))
-        //    {
-        //        // why not openasync?
-        //        conn.Open();
-        //        await connectionFunction(conn);
-        //    }
-        //}
-
-
-        //public static async Task<IDbConnection> GetOpenConnectionAsync()
-        //{
-        //    var connection = new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=IMDBChallenge;Trusted_Connection=True;MultipleActiveResultSets=true");
-        //    await connection.OpenAsync();
-        //    return connection;
-        //}
-
-        //// Wrapper
-        //// Return a tuple with the int output of the inner function, and the elapsedMilliseconds of this WithTimer wrapper
-        //// pass in the string input to the inner function
-        //public static (int, string) WithTimer(string input, Func<string, int> function)
-        //{
-        //    var sw = Stopwatch.StartNew();
-        //    int output = function(input);
-        //    return (output, sw.ElapsedMilliseconds.ToString());
-        //}
-
-        //// Function that DoesSomething
-        //public static int DoSomething(string input)
-        //{
-        //    Thread.Sleep(500);
-        //    return 2;
-        //}
-
-        // Example 1
-        //public static void Main()
-        //{
-        //    // Calling code
-        //    var (output, elapsedMilliseconds) = WithTimer("input for DoSomething", DoSomething);
-        //    Console.WriteLine($"output is: {output} which took: {elapsedMilliseconds}ms");
-        //}
-
-        //// Wrapper
-        //// Return a tuple with the int output of the inner function, and the elapsedMilliseconds of this WithTimer wrapper
-        //// pass in the string input to the inner function
-        //public static (int, string) WithTimer(string input, Func<string, int> function)
-        //{
-        //    var sw = Stopwatch.StartNew();
-        //    int output = function(input);
-        //    return (output, sw.ElapsedMilliseconds.ToString());
-        //}
-
-        //// Function that DoesSomething
-        //public static int DoSomething(string input)
-        //{
-        //    Thread.Sleep(500);
-        //    return 2;
-        //}
         public class Actor
         {
             public int actorid { get; set; }
@@ -152,7 +69,5 @@ namespace MPActorsConsole
 
             public override string ToString() => $"{actorid} {name} {sex}";
         }
-
-
     }
 }
